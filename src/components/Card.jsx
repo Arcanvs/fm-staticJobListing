8f14f5ef7e8a5d1c7b2d853e7da1440d45e0c892
Jose <jose.osorio.herrera@gmail.com>
2023-01-21 03:22:31 -0300

    [UPD] estilos card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 const Card = ({jobCard}) => {
-    const {company, position, postedAt, contract, location} = jobCard;
+    const {company, position, postedAt, contract, location, logo} = jobCard;
     return (
     <div className="card__content job">
         <div className="card__body featured">
             <div className="body__content">
                 <div className="data">
                     <div className="data__foto">
-                        Foto
+                        <img src={`../../public/${logo}`} alt={company} />
                     </div>
                     <div className="data__job">
                         <div>
-                            {company}
+                            <span className="job__company">{company}</span>
                             <span>new</span>
                             <span>featured</span>
                         </div>
