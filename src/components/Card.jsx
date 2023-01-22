bf64c0bdac80f566b1c5cd595a0b599220e92439
Jose <jose.osorio.herrera@gmail.com>
2023-01-22 02:34:23 -0300

    [UPD] maquetacion Desktop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,35 @@
 const Card = ({jobCard}) => {
-    const {company, position, postedAt, contract, location, logo} = jobCard;
+    const classes = ["card__body", jobCard.featured ? "featured" : null].join(" ");
+    
     return (
     <div className="card__content job">
-        <div className="card__body featured">
+        <div className={classes}>
             <div className="body__content">
                 <div className="data">
                     <div className="data__foto">
-                        <img src={`../../public/${logo}`} alt={company} />
+                        <img src={`../../public/${jobCard.logo}`} alt={jobCard.company} />
                     </div>
                     <div className="data__job">
                         <div>
-                            <span className="job__company">{company}</span>
-                            <span>new</span>
-                            <span>featured</span>
+                            <span className="job__company">{jobCard.company}</span>
+                            { jobCard.new && <div className="job__badge new"><span>NEW!</span></div> }
+                            { jobCard.featured && <div className="job__badge featured"><span>FEATURED</span></div> }
+                            
                         </div>
-                        <h1>{position}</h1>
-                        <div>
-                            <span>{postedAt}</span>
-                            <span>{contract}</span>
-                            <span>{location}</span>
+                        <h1>{jobCard.position}</h1>
+                        <div className="job__at">
+                            <span>{jobCard.postedAt}</span>
+                            <span>{jobCard.contract}</span>
+                            <span>{jobCard.location}</span>
                         </div>
                     </div>
                 </div>
                 <div className="tags">
-                    {company}
+                    { jobCard.role && <div><span>{jobCard.role}</span></div> }
+                    { jobCard.level && <div><span>{jobCard.level}</span></div> }
+                    { jobCard.languages.map((lang, index) => {
+                        return <div key={index} ><span>{lang}</span></div>
+                    })} 
                 </div>
             </div>
         </div>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,20 @@
+import btnRemove from '../assets/icon-remove.svg';
+
 const Filter = ({selectedDetail}) => {
   return (
     <div className="card__content filter">
         <div className={!selectedDetail ? 'card__hide' : 'card__body' }>
-            Filtro jobs
+            <div className="body__content filter">
+              <div className="buttons">
+                <span>
+                  Frontend
+                  <img src={btnRemove} alt="ico_romve" />
+                </span>
+                <span>CSS</span>
+                <span>JavasScript</span>
+              </div>
+              <span>Clear</span>
+            </div>
         </div>
     </div>
   )
