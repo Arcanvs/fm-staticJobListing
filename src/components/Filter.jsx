c2765ba1ac315c9c6da807fc958e7b85ec048ac6
Jose <jose.osorio.herrera@gmail.com>
2023-01-25 02:29:12 -0300

    estilos botones filtros

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -7,23 +7,37 @@ const Filter = ({selectedDetail, onClearAllTags, onClearTag}) => {
   const handleClearFilter = (delTag) => {
     onClearTag(delTag);
   }
+  /* 
+  <span key={index}>
+                        {tag}
+                        <img src={btnRemove} alt="ico_romve" onClick={() => handleClearFilter(tag)} />
+                      </span>
+  */
   return (
-    <div className="card filter">
-        <div className={selectedDetail.length > 0 ? 'card__body' : 'card__hide' }>
+    <div className="filter">
+        <div className={selectedDetail.length > 0 ? 'card' : 'card__hide' }>
+          <div className='card__body filter'>
             <div className="body__content filter">
-              <div className="buttons">
-                { selectedDetail && selectedDetail.map((tag, index) => {
-                  return(
-                    <span key={index}>
-                      {tag}
-                      <img src={btnRemove} alt="ico_romve" onClick={() => handleClearFilter(tag)} />
-                    </span>
-                  )
-                })}
+                <div className="button__group">
+                  { selectedDetail && selectedDetail.map((tag, index) => {
+                    return(
+                      <div className='button__filter' key={index}>
+                        <div className='button__title'>
+                          {tag}
+                        </div>  
+                        <div className='button__ico'>
+                          <img src={btnRemove} alt="ico_romve" onClick={() => handleClearFilter(tag)} />
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+                <div className='button__clear'>
+                  <span onClick={handleClearAllFilter}>Clear</span>
+                </div>
               </div>
-              <span onClick={handleClearAllFilter}>Clear</span>
-            </div>
-        </div>
+          </div>
+          </div>  
     </div>
   )
 }
